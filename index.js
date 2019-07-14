eb5730d333c8c4b0eba10ebcf3965a67cb2f2e76
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-14 18:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 34
-var message = ` is ${height + 40} inches tall`
+var message = `${name} is ${height + 40} inches tall`
 
 module.exports = {name, height, message}
